de80b39b5512179b0df7894e6b281fdcd0fa6865
HansSima <SimaHonza@seznam.cz>
2025-07-07 17:17:29 +0200

    init commit

--- src/main/java/org/example/TutorialPointCheckBoxTest.java
+++ src/main/java/org/example/TutorialPointCheckBoxTest.java
@@ -3,9 +3,11 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TutorialPointCheckBoxTest {
 
@@ -17,14 +19,17 @@ public class TutorialPointCheckBoxTest {
         //driver = new FirefoxDriver();
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
     }
 
     @Test
     public void test(){
+        //Prepne se na pozadovanou stranku
         driver.get("https://www.tutorialspoint.com/selenium/practice/check-box.php");
-
-
+        //Najde prvkek a klikne na nej
+        driver.findElement(By.id("c_bs_1")).click();
+        //assertEquals();
+        //Kontroluje zda je prvek zaskrtnut
+        assertTrue(driver.findElement(By.id("c_bs_1")).isSelected());
     }
 
 }
