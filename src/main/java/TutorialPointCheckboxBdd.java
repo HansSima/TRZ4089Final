b44e2deabaee34fde01f69e7dcf95aead67226ea
HansSima <SimaHonza@seznam.cz>
2025-07-07 19:21:28 +0200

    Add BDD

---
+++ src/main/java/TutorialPointCheckboxBdd.java
@@ -0,0 +1,46 @@
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TutorialPointCheckboxBdd {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        //WebDriverManager.firefoxdriver().setup();
+        //driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void cleanUp(){
+        driver.quit();
+    }
+
+
+    @Given("Uzivatel se nachazi na strance TutorialPoint checkbox")
+    public void uzivatelSeNachaziNaStranceTutorialPointCheckbox() {
+        driver.get("https://www.tutorialspoint.com/selenium/practice/check-box.php");
+    }
+
+    @When("Uzivatel klikne na checkbox {string}")
+    public void uzivatelKlikneNaCheckbox(String idCheckbox) {
+        driver.findElement(By.id(idCheckbox)).click();
+    }
+
+    @Then("Checkbox {string} je zaskrtnut")
+    public void checkboxJeZaskrtnut(String idCheckbox) {
+        assertTrue(driver.findElement(By.id(idCheckbox)).isSelected());
+    }
+}
---
+++ src/main/resources/TutorialPointCheckbox.feature
@@ -0,0 +1,6 @@
+Feature: TutorialPointCheckbox
+
+  Scenario: Checkbox zakladni zaskrtnuti
+    Given Uzivatel se nachazi na strance TutorialPoint checkbox
+    When Uzivatel klikne na checkbox "c_bs_1"
+    Then Checkbox "c_bs_1" je zaskrtnut
